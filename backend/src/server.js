500a04f8c7f61544b93ef7389f9978fa287dcccb
unknown <shakeelrehman501@gmail.com>
2026-09-22 20:13:35 +0500

    Connect MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import app from './app.js'
 import 'dotenv/config'
+import connectDB from './database/db.js';
 
 const PORT = process.env.PORT || 3000;
-const startServer = ()=>{
+const startServer = async()=>{
     try {
+        await connectDB()
         app.listen(PORT, (req, res)=>{
             console.log(`Server created at port: ${PORT}`);
         })
