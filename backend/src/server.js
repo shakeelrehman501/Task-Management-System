97d96e9a38b1a8ad02829f4b244826fa9b6d17c0
unknown <shakeelrehman501@gmail.com>
2026-09-22 20:28:02 +0500

    Create user schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,17 @@
-import app from './app.js'
-import 'dotenv/config'
-import connectDB from './database/db.js';
+import app from "./app.js";
+import "dotenv/config";
+import connectDB from "./database/db.js";
 
 const PORT = process.env.PORT || 3000;
-const startServer = async()=>{
-    try {
-        await connectDB()
-        app.listen(PORT, (req, res)=>{
-            console.log(`Server created at port: ${PORT}`);
-        })
-    } catch (error) {
-        console.log("Server error: " + error);
-    }
-}
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, (req, res) => {
+      console.log(`Server created at port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Server error: " + error);
+  }
+};
 
-startServer()
+startServer();
