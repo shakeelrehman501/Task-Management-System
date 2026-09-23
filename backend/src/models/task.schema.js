80fafbdcb9b8e5062b641aed7fc6ec8bb5fa400e
unknown <shakeelrehman501@gmail.com>
2026-09-23 10:21:06 +0500

    Create task schema

---
+++ backend/src/models/task.schema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim:true },
+  description: { type: String, trim: true },
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Completed"],
+    default: "Pending",
+  },
+
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Medium",
+  },
+  due_Date: { type: Date },
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+  },
+
+  
+}, {timestamps:true});
+
+const Tasks = mongoose.model("Tasks", taskSchema);
+
+export default Tasks;
